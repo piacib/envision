4671370774c6c716417a78805cc0eeb714dc7d4a
piacib <ben.piacitelli@gmail.com>
2023-11-07 11:46:59 -0800

    added custom button to copy addendum data

--- scripts/content.js
+++ scripts/content.js
@@ -1,5 +1,12 @@
-(() => {
-  console.log("content scritp");
-  //   const a = document.getElementsByTagName("a");
-  navigator.clipboard.writeText("testing service worker");
-})();
+console.log("content script");
+let container = document.createElement("div");
+let button = document.createElement("button");
+
+container.append(button);
+button.innerText = "Copy Addendum Data";
+button.addEventListener("click", () => {
+  // const
+  navigator.clipboard.writeText("test\ntest\ntest\t1\t2");
+});
+console.log(document.getElementsByTagName("h1")[0]);
+document.getElementsByTagName("h1")[0].after(container);
